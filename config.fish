584fb518b3f4d715d86c0a2fa994c53e45a94b80
Lior Gleizer <100088104+liorg2007@users.noreply.github.com>
2026-03-03 22:34:03 +0200

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,12 +1,1 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
 
-function fish_greeting; end
-
-export PATH="$PATH:$HOME/jadx/build/jadx/bin"
-export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
-
-alias luamake="/home/zera/.local/lua-language-server/3rd/luamake/luamake"
-export PATH="$HOME/.local/lua-language-server/bin/Linux:$PATH"
-export PATH="/home/zera/opt/cross/bin:$PATH"
